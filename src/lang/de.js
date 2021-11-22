04d805a8226d482680b557ffe2ce95338923be53
Berk Altıok <berkpw@gmail.com>
2021-11-23 00:48:16 +0300

    feat(i18n): add DE language

---
+++ src/lang/de.js
@@ -0,0 +1,45 @@
+export default {
+  info: {
+    name: "Deutsch",
+    languages: {
+      tr: "Almanca",
+      en: "German",
+      de: "Deutsch"
+    }
+  },
+  search: {
+    title: "Benutzer ID / Beliebige ID",
+    input: {
+      required: "Dieses Feld ist benötigt.",
+      minLength: "Muss mindestens {0} Zeichen lang sein.",
+      maxLength: "Darf maximal {0} Zeichen lang sein.",
+      numeric: "Es sind nur Zahlen (Discord ID) akzeptiert."
+    },
+    button: "Suchen",
+    quick: "Benutzer ID oder beliebige ID"
+  },
+  list: {
+    recent: "Letzte Suchen",
+    favorites: "Favoriten",
+    empty: "Diese Liste ist leer",
+    approve: "Entfernen"
+  },
+  result: {
+    title: {
+      id: "Benutzer ID",
+      type: "Benutzertyp",
+      date: "Erstellungsdatum",
+      age: "Alter"
+    },
+    type: {
+      user: "Normales Konto",
+      bot: "Bot-Konto"
+    },
+    favorites: {
+      add: "Als Favorit markieren",
+      remove: "Favoriten entfernen"
+    }
+  },
+  selectLanguage: 'Sprache',
+  discord: "Nicht mit Discord, Inc. verbunden."
+}
