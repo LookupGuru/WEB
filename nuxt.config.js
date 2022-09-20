fb4be59cf65a90ebdb4b79ff4375aafa9105dc29
Berk Altıok <berkpw@gmail.com>
2022-09-20 08:20:07 +0300

    chore: add meta tags i18n

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,40 +1,48 @@
 export default {
-  head: {
-    title: 'Discord Search ID',
-    titleTemplate: '%s - Lookup.Guru',
-    meta: [
-      { charset: 'utf-8' },
-      {
-        name: 'viewport',
-        content:
-          'width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no,viewport-fit=cover'
-      },
-      { name: 'format-detection', content: 'telephone=no' },
+  head() {
+    const i18nHead = this.$nuxtI18nHead({ addSeoAttributes: true })
 
-      {
-        rel: 'search',
-        type: 'application/opensearchdescription+xml',
-        title: 'lookup.guru',
-        href: '/opensearch.xml?v2'
+    return {
+      title: 'Discord Search ID',
+      titleTemplate: '%s - Lookup.Guru',
+      htmlAttrs: {
+        ...i18nHead.htmlAttrs
       },
+      meta: [
+        { charset: 'utf-8' },
+        {
+          name: 'viewport',
+          content:
+            'width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no,viewport-fit=cover'
+        },
+        { name: 'format-detection', content: 'telephone=no' },
 
-      { rel: 'canonical', href: 'https://lookup.guru/' },
+        {
+          rel: 'search',
+          type: 'application/opensearchdescription+xml',
+          title: 'lookup.guru',
+          href: '/opensearch.xml?v2'
+        },
 
-      { hid: 'title', name: 'title', content: 'Discord Search ID - Lookup.Guru' },
-      {
-        hid: 'description',
-        name: 'description',
-        content:
-          'Lookup.Guru is a free and open source Discord search engine. It allows you to search for Discord IDs and get information about them.'
-      },
-      {
-        hid: 'keywords',
-        name: 'keywords',
-        content:
-          'Discord Search ID, Discord Lookup, Discord ID Lookup, Discord ID Finder, Discord Look Up, Discord ID Tracker, Discord ID Search, Discord ID, Discord Search, Lookup.Guru, Lookup Guru, Discord, Search, ID, Lookup, Guru'
-      }
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }]
+        { rel: 'canonical', href: 'https://lookup.guru/' },
+
+        { hid: 'title', name: 'title', content: 'Discord Search ID - Lookup.Guru' },
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'Lookup.Guru is a free and open source Discord search engine. It allows you to search for Discord IDs and get information about them.'
+        },
+        {
+          hid: 'keywords',
+          name: 'keywords',
+          content:
+            'Discord Search ID, Discord Lookup, Discord ID Lookup, Discord ID Finder, Discord Look Up, Discord ID Tracker, Discord ID Search, Discord ID, Discord Search, Lookup.Guru, Lookup Guru, Discord, Search, ID, Lookup, Guru'
+        },
+        ...i18nHead.meta
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }, ...i18nHead.link]
+    }
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -89,31 +97,38 @@ export default {
     locales: [
       {
         code: 'en',
-        file: 'en-US.js'
+        file: 'en-US.js',
+        iso: 'en-US'
       },
       {
         code: 'tr',
-        file: 'tr-TR.js'
+        file: 'tr-TR.js',
+        iso: 'tr-TR'
       },
       {
         code: 'de',
-        file: 'de-DE.js'
+        file: 'de-DE.js',
+        iso: 'de-DE'
       },
       {
         code: 'fr',
-        file: 'fr-FR.js'
+        file: 'fr-FR.js',
+        iso: 'fr-FR'
       },
       {
         code: 'ru',
-        file: 'ru-RU.js'
+        file: 'ru-RU.js',
+        iso: 'ru-RU'
       },
       {
         code: 'dk',
-        file: 'dk-DK.js'
+        file: 'dk-DK.js',
+        iso: 'dk-DK'
       },
       {
         code: 'pt',
-        file: 'pt-PT.js'
+        file: 'pt-PT.js',
+        iso: 'pt-PT'
       }
     ],
     langDir: 'lang/',
