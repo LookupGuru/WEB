8b98b85b0a75e87b087a725a794858adc47ee973
Berk Altıok <17373485+berkaltiok@users.noreply.github.com>
2023-01-09 17:24:29 +0300

    fix: id checker

--- middleware/check.js
+++ middleware/check.js
@@ -1,6 +1,6 @@
 export default function ({ route, redirect, store }) {
   if (!route.params.id) return
-  if (!/^\d+$/.test(route.params.id)) {
+  if (!/^\d+$/.test(route.params.id) && route.params.id.length < 23 && route.params.id.length > 14) {
     store.commit('setResult', null)
     store.commit('setLoading', false)
     return redirect('/')
